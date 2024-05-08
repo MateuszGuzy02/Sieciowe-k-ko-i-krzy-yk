92ca452bb9a9cc97a1dbc81760500061146a419d
GuzyMateusz <guzymateusz02@gmail.com>
2024-05-08 12:56:08 +0200

    Update

---
+++ Klient/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Klient
+{
+    class Program
+    {
+        static TcpClient client;
+        static NetworkStream stream;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w grze Kółko i Krzyżyk!");
+
+            Console.Write("Podaj adres IP serwera: ");
+            string serverIP = Console.ReadLine();
+            //string serverIP = "192.168.0.216";
+
+            client = new TcpClient(serverIP, 8888);
+            stream = client.GetStream();
+
+            Console.WriteLine("Połączono z serwerem.");
+
+
+            // Odbieranie informacji o planszy
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+
+            // Rozpoczęcie pętli gry
+            while (true)
+            {
+                // Odbieranie informacji o turze gracza
+                bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                // Obsługa różnych komunikatów od serwera
+                if (message.StartsWith("YourTurn"))
+                {
+                    Console.Write("Twoja kolej. Podaj numer pola (1-9): ");
+                    int choice = GetPlayerChoice();
+
+                    byte[] choiceData = Encoding.ASCII.GetBytes(choice.ToString());
+                    stream.Write(choiceData, 0, choiceData.Length);
+                }
+
+                else if (message.StartsWith("OpponentTurn"))
+                    Console.WriteLine("Oczekiwanie na ruch przeciwnika...");
+
+                else if (message.StartsWith("Wygrywa gracz nr.") || message.StartsWith("Remis"))
+                {
+                    Console.WriteLine("Gra zakończona. Wynik: " + message);
+                    break;
+                }
+
+                else
+                    Console.WriteLine(message);
+
+            }
+
+            stream.Close();
+            client.Close();
+        }
+
+        static int GetPlayerChoice()
+        {
+            int choice = 0;
+            bool isValidChoice = false;
+
+            while (!isValidChoice)
+            {
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out choice))
+                {
+                    if (choice >= 1 && choice <= 9)
+                        isValidChoice = true;
+
+                    else
+                        Console.WriteLine("Podano niepoprawny numer pola. Wpisz liczbę od 1 do 9.");
+                }
+
+                else
+                    Console.WriteLine("Podano niepoprawną wartość. Wpisz liczbę od 1 do 9.");
+            }
+            return choice;
+        }
+    }
+}
